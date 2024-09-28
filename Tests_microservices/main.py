cf60e2e253b7a5df0ba289d65ea7d87af65da7ed
Hariela <135130433+Hariela@users.noreply.github.com>
2024-09-28 21:48:15 +0300

    Update main.py

--- Tests_microservices/main.py
+++ Tests_microservices/main.py
@@ -6,50 +6,50 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Salut"
-# # Route pour ajouter un livre
-# @app.route('/books', methods=['POST'])
-# def add_book():
-#     data = request.get_json()
-#     title = data['title']
-#     author = data['author']
-#     published_date = data.get('published_date')
-#     isbn = data['isbn']
+# Route pour ajouter un livre
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    title = data['title']
+    author = data['author']
+    published_date = data.get('published_date')
+    isbn = data['isbn']
 
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('INSERT INTO books (title, author, published_date, isbn) VALUES (%s, %s, %s, %s)',
-#                    (title, author, published_date, isbn))
-#     mysql.connection.commit()
-#     cursor.close()
-#     return jsonify({'message': 'Book added successfully!'}), 201
+    cursor = mysql.connection.cursor()
+    cursor.execute('INSERT INTO books (title, author, published_date, isbn) VALUES (%s, %s, %s, %s)',
+                   (title, author, published_date, isbn))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'Book added successfully!'}), 201
 
-# # Route pour récupérer tous les livres
-# @app.route('/books', methods=['GET'])
-# def get_books():
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('SELECT * FROM books')
-#     books = cursor.fetchall()
-#     cursor.close()
-#     return jsonify(books), 200
+# Route pour récupérer tous les livres
+@app.route('/books', methods=['GET'])
+def get_books():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM books')
+    books = cursor.fetchall()
+    cursor.close()
+    return jsonify(books), 200
 
-# # Route pour modifier un livre
-# @app.route('/books/<int:id>', methods=['PUT'])
-# def update_book(id):
-#     data = request.get_json()
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('UPDATE books SET title = %s, author = %s, published_date = %s, isbn = %s WHERE id = %s',
-#                    (data['title'], data['author'], data.get('published_date'), data['isbn'], id))
-#     mysql.connection.commit()
-#     cursor.close()
-#     return jsonify({'message': 'Book updated successfully!'}), 200
+# Route pour modifier un livre
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    data = request.get_json()
+    cursor = mysql.connection.cursor()
+    cursor.execute('UPDATE books SET title = %s, author = %s, published_date = %s, isbn = %s WHERE id = %s',
+                   (data['title'], data['author'], data.get('published_date'), data['isbn'], id))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'Book updated successfully!'}), 200
 
-# # Route pour supprimer un livre
-# @app.route('/books/<int:id>', methods=['DELETE'])
-# def delete_book(id):
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('DELETE FROM books WHERE id = %s', (id,))
-#     mysql.connection.commit()
-#     cursor.close()
-#     return jsonify({'message': 'Book deleted successfully!'}), 200
+# Route pour supprimer un livre
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM books WHERE id = %s', (id,))
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'Book deleted successfully!'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
